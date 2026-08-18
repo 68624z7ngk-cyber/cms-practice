ec41958fc03e739aff6d6105662ebffac5ef8cd7
jimu <j@example.com>
2026-08-18 15:35:31 +0900

    Portfolioを複数表示できるようにした

--- site_content/models.py
+++ site_content/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Portfolio(models.Model):
     title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to="portfolio/", blank=True, null=True)
 
     def __str__(self):
         return self.title
